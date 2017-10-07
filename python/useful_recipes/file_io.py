dbe8d13b32014fb10672decc0213304d94c7aed0
Rohan Budhiraja <proyan@users.noreply.github.com>
2017-10-07 20:08:30 +0200

    [file-io] write zipped list to file

--- python/useful_recipes/file_io.py
+++ python/useful_recipes/file_io.py
@@ -46,13 +46,52 @@ def writeXYZasCSVFile(filename, x,y,z=None, DELIMITOR = ", "):
         file_xyz.write(line)
     file_xyz.close()
 
-def parseJointStatesFromFile(filename, DELIMITOR=" "):
+def writeArraysAsCSVFile(filename, ar_list, DELIMITOR = ", "):
+    """
+    Write list of arrays as CSV file. Default Delimitor is comma
+
+    input: (string) filename, (numpy array) x, (numpy array) y, (numpy array) z,
+           (string) DELIMITOR = ", "
+    output: void
+
+
+    """
+    NEWLINE = "\n"
+    file_xyz = open(filename, "w")
+    tup_list = zip(*ar_list)
+    for tup in tup_list:
+        line = ""
+        for ind,val in enumerate(tup):
+            if ind == len(tup)-1:
+                line+= str(val)+NEWLINE
+            else:
+                line+=str(val)+DELIMITOR
+        file_xyz.write(line)
+    file_xyz.close()
+
+def parseJointStatesFromFile(filename):
     """Inputs a file and ouputs a matrix.
-    input: (string) filenam, (string) DELIMITOR=" "
+    input: (string) filename
     output: (numpy array) seq
     """
     import pandas as pd
     import numpy as np
-    dt = 0.001
     seq = np.array(pd.read_csv(filename,header=None, delim_whitespace=True)).T
     return seq
+
+def writeZippedListToFile(to_write, filename, DELIMITOR = " "):
+
+  """Write List to file. Default delimitor is space                                                   
+  """
+  file_q = open(filename,"w")
+  NEWLINE = "\n"
+  for input_line in to_write:
+    output_line = ""
+    n = len(input_line)
+    for i in xrange(n):
+      if i == n-1:
+        output_line += str(input_line[i]) + NEWLINE
+      else:
+        output_line += str(input_line[i]) + DELIMITOR
+    file_q.write(output_line)
+  file_q.close()
